788c026117c506ab64989f3eaa5137f850c452bd
Brian Wangila <bwanjalah@gmail.com>
2022-09-09 14:20:00 +0300

    create EditReview component

--- src/components/EditReview.js
+++ src/components/EditReview.js
@@ -1,1 +1,21 @@
+import React, { useState } from "react";
 
+function EditMechanic({ id, onUpdateReview}){
+ 
+
+
+
+
+
+  return (
+    <div>
+      <form className="" onSubmit={handleFormSubmit}>
+        <input type="text" name="body" value={comment} onChange={(e) => setComment(e.target.value)}/>
+        <input type="text" name="body" value={rating} onChange={(e) => setRating(e.target.value)}/>
+
+        <input type="submit" value="Save Details"/>
+      </form>
+    </div>
+  )
+}
+export default EditMechanic;
