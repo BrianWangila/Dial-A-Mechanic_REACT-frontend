737576b0184afc051f71e571f78d033f729d22a5
Brian Wangila <bwanjalah@gmail.com>
2022-09-09 14:22:54 +0300

    add DeleteReview component for deleting reviews

--- src/components/DeleteReview.js
+++ src/components/DeleteReview.js
@@ -1,1 +1,18 @@
+import React from "react";
 
+function DeleteReview(){
+
+
+
+  
+
+  return (
+    <div>
+      <span onClick={deleteReview} role="img" aria-label="delete" type="button"> 
+      🗑 Delete
+      </span>
+    </div>
+  )
+}
+
+export default DeleteReview;
