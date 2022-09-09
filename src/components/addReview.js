087a7ccf1b0287cd5c182a97aa9b4d74d57228a6
Brian Wangila <bwanjalah@gmail.com>
2022-09-09 14:27:37 +0300

    add form for adding new review

--- src/components/addReview.js
+++ src/components/addReview.js
@@ -10,7 +10,19 @@ function AddReview({onAddReview, id}){
 
   return (
     <div>
-     
+      <form className="add-review" form onSubmit={handleAddReview}>
+      <input type="text" className="form-control" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
+      <select className="form-select form-select-sm" onChange={(e) => setRating(e.target.value)}>
+          <option>Rating</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <textarea type="textArea" className="form-control" rows="3" placeholder="Comment" onChange={(e) => setComment(e.target.value)}></textarea>
+        <button type="submit">Add a Review</button>
+      </form>
     </div>
   )
 }
